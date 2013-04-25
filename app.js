c7917f4c08a1cce95790535d837c8742280c6299
Tomás García <tomas.garcia.tinoco@gmail.com>
2013-04-25 22:16:47 +0200

    Initial submit

--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 /**
  * Module dependencies.
  */
+ 
+ // For cloud foundry
+require("cf-autoconfig");
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
+	, routes = require('./routes')
+	, redis = require('node-redis')
+	, http = require('http')
+	, app = express()
+	, server = http.createServer(app)
+	, sio = require('socket.io').listen(server, {
+		log: false
+	})
+	, path = require('path');
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.VCAP_APP_PORT || process.env.PORT || 3000);
 
 // carpeta de vistas
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +32,7 @@ app.use(express.favicon());
 // Activamos log a nivel de desarrollo
 app.use(express.logger('dev'));
 
-// para conocer el tipo de petición 
+// para conocer el tipo de petición (multipart / url-encoded)
 app.use(express.bodyParser());
 
 // si el navegador no soporta algunos metodos http como PUT, se usa un parámetro y fuerza el método
@@ -40,20 +46,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Añadimos less-middleware para compilar los ficheros .less a .css
 app.use(require('less-middleware')({
-    src: __dirname + '/public',
-    compress: 'auto',
-    force: true 
+		src: __dirname + '/public',
+		compress: 'auto',
+		force: true 
 }));
 
 // configuración para el entorno de desarrollo
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
 // Acción a ejecutar al acceder al raiz
 app.get('/', routes.index);
 
+app.clientes = 0;
+sio.sockets.on('connection', function(socket) {
+	socket.on('disconnect', function() {
+		console.log('Se ha ido un cliente, ya somos', --app.clientes);
+	});
+	console.log('Se ha conectado un cliente, ya somos', ++app.clientes);
+});
+
 // Iniciamos el servidor
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
